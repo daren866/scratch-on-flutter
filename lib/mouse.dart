c51d0807dbf94c2038d49c6900131c8718bd7b35
daren866 <xiao1xiao188@qq.com>
2026-05-30 10:08:53 +0800

    添加Mouse类调试日志

--- lib/mouse.dart
+++ lib/mouse.dart
@@ -14,20 +14,25 @@ class Mouse {
   bool get isDown => _isDown;
 
   void postData(Map<String, dynamic> data) {
+    print('Mouse.postData 收到数据: $data');
+    
     if (data.containsKey('x')) {
       _clientX = _toDouble(data['x']);
       final canvasWidth = _toDouble(data['canvasWidth'] ?? 480);
       _scratchX = _toScratchX(_clientX, canvasWidth);
+      print('更新鼠标 X: 客户端=$_clientX → Scratch=$_scratchX');
     }
 
     if (data.containsKey('y')) {
       _clientY = _toDouble(data['y']);
       final canvasHeight = _toDouble(data['canvasHeight'] ?? 360);
       _scratchY = _toScratchY(_clientY, canvasHeight);
+      print('更新鼠标 Y: 客户端=$_clientY → Scratch=$_scratchY');
     }
 
     if (data.containsKey('isDown')) {
       _isDown = data['isDown'] as bool;
+      print('更新鼠标按下状态: $_isDown');
     }
   }
 
@@ -53,18 +58,25 @@ class Mouse {
   }
 
   dynamic ioQuery(String query) {
+    print('Mouse.ioQuery 被调用: $query');
     switch (query) {
       case 'getScratchX':
+        print('返回 scratchX: $_scratchX');
         return _scratchX;
       case 'getScratchY':
+        print('返回 scratchY: $_scratchY');
         return _scratchY;
       case 'getIsDown':
+        print('返回 isDown: $_isDown');
         return _isDown;
       case 'getClientX':
+        print('返回 clientX: $_clientX');
         return _clientX;
       case 'getClientY':
+        print('返回 clientY: $_clientY');
         return _clientY;
       default:
+        print('未知查询: $query');
         return null;
     }
   }
